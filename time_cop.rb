d340887edd55c4f9c316d5504988f067a2740fa5
Hunt Norment <hunt@sherpa.red>
2015-09-04 09:13:50 -0500

    timekeeper method

--- time_cop.rb
+++ time_cop.rb
@@ -1,6 +1,23 @@
 require 'timeout'
+require 'open-uri'
 
-Timeout.timeout(2.0) do
-  sleep 1.0
-  puts 'That was refreshing...'
+def try_with_timeout(duration)
+  begin
+    Timeout.timeout(duration) do
+      yield
+    end
+  rescue Timeout::Error
+    puts 'Took too long!'
+  end
 end
+
+try_with_timeout(2.0) do
+  sleep 1.0
+  puts 'That was refreshing.'
+end
+
+web_page = try_with_timeout(1.0) do
+  open('http://example.com')
+end
+
+puts web_page.read
